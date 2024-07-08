e113ef9c431b877a418597abe30ebeda47748b98
Tiago Kretzer Walter <tiagokwalter@gmail.com>
2024-07-08 11:46:19 -0300

    implemented temporary login

--- my_investments_app/views.py
+++ my_investments_app/views.py
@@ -1,11 +1,16 @@
-from rest_framework import generics
-from .serializers import UserSerializer
+from rest_framework import generics, status
+from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from django.contrib.auth import authenticate, login
+from django.views.decorators.csrf import csrf_exempt
+from .serializers import UserSerializer, UserLoginSerializer
+
 
 class CreateUserView(generics.CreateAPIView):
     """Create a new user in the system"""
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
+
 
 class UpdateUserView(generics.UpdateAPIView):
     serializer_class = UserSerializer
@@ -13,3 +18,21 @@ class UpdateUserView(generics.UpdateAPIView):
 
     def get_object(self):
         return self.request.user
+    
+
+class UserLoginView(generics.GenericAPIView):
+    serializer_class = UserLoginSerializer
+    permission_classes = [AllowAny]
+    
+    @csrf_exempt
+    def post(self, request):
+        email = request.data.get('email')
+        password = request.data.get('password')
+        
+        user = authenticate(request, email=email, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return Response({"message": "Login successful."})
+        else:
+            return Response({"error": "Invalid credentials."}, status=status.HTTP_401_UNAUTHORIZED)
