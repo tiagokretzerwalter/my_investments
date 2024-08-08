d520d68176e3602f826c7f64c7c40c0314949777
Tiago <tiagokwalter@gmail.com>
2024-08-08 11:45:08 -0300

    register admin for crypto

--- investments_types/admin.py
+++ investments_types/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import CryptoInvestments, SoldCrypto
 
-# Register your models here.
+admin.site.register(CryptoInvestments)
+admin.site.register(SoldCrypto)
