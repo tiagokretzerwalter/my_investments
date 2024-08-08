f337c91065a66b085652f8b29aae6066a8bc50fd
Tiago <tiagokwalter@gmail.com>
2024-08-08 07:21:58 -0300

    create test_views for crypto

--- my_investments_app/admin.py
+++ my_investments_app/admin.py
@@ -7,22 +7,16 @@ from . import models
 
 
 class CustomUserAdmin(BaseUserAdmin):
-    ordering = ['id']
-    list_display = ['email', 'name']
+    ordering = ["id"]
+    list_display = ["email", "name"]
     fieldsets = (
-        (None, {"fields": ('email', 'password')}),
-        (_('Personal Info'), {'fields': ('name',)}),
-        (
-            _('Permissions'),
-            {'fields': ('is_active', 'is_staff', 'is_superuser')}
-        ),
-        (_('Important dates'), {'fields': ('last_login',)})
+        (None, {"fields": ("email", "password")}),
+        (_("Personal Info"), {"fields": ("name",)}),
+        (_("Permissions"), {"fields": ("is_active", "is_staff", "is_superuser")}),
+        (_("Important dates"), {"fields": ("last_login",)}),
     )
     add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2')
-        }),
+        (None, {"classes": ("wide",), "fields": ("email", "password1", "password2")}),
     )
 
 
